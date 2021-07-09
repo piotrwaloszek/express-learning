4d8af55c0a968e4d2738d7c592569f4ead5c6d58
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-07-09 13:33:53 +0200

    Handlebars

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: 'dark' });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+  })
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
